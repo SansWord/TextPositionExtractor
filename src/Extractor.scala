cab2fbbe34efcc193d8f6c8fcfb2c34d7197f6b6
Yung-Luen Lan <yllan@me.com>
2014-03-19 01:33:09 +0800

    抓所有 PDF

--- src/Extractor.scala
+++ src/Extractor.scala
@@ -6,27 +6,54 @@ import org.apache.pdfbox.pdmodel.common.PDStream
 import org.apache.pdfbox.util._
 
 import scala.collection.JavaConverters._
+import scala.io._
+import java.io._
+
+case class PDFFile(id: Int, file: String, page: Int)
 
 object Extractor extends App {
-  val doc = PDDocument.load("test.pdf")
-  val printer = new PrintTextLocations()
+  val lines = Source.fromFile("output.csv").getLines.toList
+  val files = lines.map(line => {
+    val Array(id, file, page) = line.split(",").take(3)
+    
+    PDFFile(id.toInt, "pdf/" + (if (file.startsWith("\"") && file.endsWith("\"")) file.drop(1).dropRight(1) else file), page.toInt)
+    
+  })
 
-  val pages: List[PDPage] = doc.getDocumentCatalog().getAllPages().asScala.toList.asInstanceOf[List[PDPage]]
-  pages.foreach((page: PDPage) => {
+  files.par.foreach((f: PDFFile) => {
+    println(s"processing: ${f.id}")
+    val doc = PDDocument.load(f.file)
+    val printer = new PrintTextLocations(new File(s"output/${f.id}.csv"))
+
+    val pages: List[PDPage] = doc.getDocumentCatalog().getAllPages().asScala.toList.asInstanceOf[List[PDPage]]
+    val page = pages(f.page)
     val contents = page.getContents
     if (contents != null) {
       printer.processStream(page, page.findResources, page.getContents.getStream)
     }
+    doc.close
   })
 }
 
-class PrintTextLocations extends PDFTextStripper {
+class PrintTextLocations(val f: File) extends PDFTextStripper {
+  val out = new PrintWriter(f, "UTF-8")
+
+  def quoteIfNeeded(s: String): String = {
+    val needToQuote = s.contains("\"") || s.contains(",")
+    if (needToQuote) {
+      "\"" + s.replace("\"", "\"\"") + "\""
+    } else s
+  }
+
   override def processTextPosition(text: TextPosition) =
   {
-    System.out.println( "String[" + text.getXDirAdj() + "," +
-            text.getYDirAdj() + " fs=" + text.getFontSize() + " xscale=" +
-            text.getXScale() + " height=" + text.getHeightDir() + " space=" +
-            text.getWidthOfSpace() + " width=" +
-            text.getWidthDirAdj() + "]" + text.getCharacter() )
+    out.println(quoteIfNeeded(text.getCharacter) + "," + 
+                text.getXDirAdj + "," + 
+                text.getYDirAdj + "," + 
+                text.getFontSize + "," +
+                text.getXScale + "," +
+                text.getHeightDir + "," +
+                text.getWidthOfSpace + "," +
+                text.getWidthDirAdj)
   }
 }
